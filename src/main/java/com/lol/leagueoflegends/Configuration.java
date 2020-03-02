08d19b86cf98d93eb047f36df8f68062bde5640c
Julio <lamajulio17@gmail.com>
2020-03-02 16:57:58 -0600

    configuration class

---
+++ src/main/java/com/lol/leagueoflegends/Configuration.java
@@ -0,0 +1,9 @@
+package com.lol.leagueoflegends;
+
+ public class Configuration {
+   private static final String RIOT_API_KEY = "RGAPI-a6f72780-4cb2-4555-8ba7-487650da0239";
+
+   public static String getRiotApiKey() {
+     return RIOT_API_KEY;
+   }
+ }
